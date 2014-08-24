f142170eab1672d99c181867dd4ac93d877eb6a0
Ciaran Kelly <ciaran.kelly@gmail.com>
2014-08-24 14:47:38 +0100

    Add ACL init

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 curl -s http://getcomposer.org/installer | php
 php composer.phar install
+HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs
